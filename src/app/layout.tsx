87db65fc40e467f2b02a060f193357f4587ed4d6
Alex4386 <psh010209@gmail.com>
2026-03-31 15:40:03 +0900

    fix: safe-zone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  viewportFit: 'cover',
+  themeColor: '#ffe66a',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
